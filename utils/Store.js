003c19d7635de49d9fc754150151820d2e668486
nunomiguens <nunomiguens@gmail.com>
2021-09-19 11:23:59 +0100

    create login page, create sample users in mongodb

--- utils/Store.js
+++ utils/Store.js
@@ -28,9 +28,16 @@ function reducer(state, action) {
           )
         : [...state.cart.cartItems, newItem];
       Cookies.set('cartItems', JSON.stringify(cartItems));
-
       return { ...state, cart: { ...state.cart, cartItems } };
     }
+    case 'CART_REMOVE_ITEM': {
+      const cartItems = state.cart.cartItems.filter(
+        item => item._id !== action.payload._id
+      );
+      Cookies.set('cartItems', JSON.stringify(cartItems));
+      return { ...state, cart: { ...state.cart, cartItems } };
+    }
+
     default:
       return state;
   }
