ee3cbe4f4dc2fef86652c9155b00471949fe58d5
nunomiguens <nunomiguens@gmail.com>
2021-09-21 13:00:24 +0100

    update for build-3

--- pages/api/orders/[id]/pay.js
+++ pages/api/orders/[id]/pay.js
@@ -1,7 +1,6 @@
 import nc from 'next-connect';
 import Order from '../../../../models/Order';
 import db from '../../../../utils/db';
-
 import { onError } from '../../../../utils/error';
 import { isAuth } from '../../../../utils/auth';
 
